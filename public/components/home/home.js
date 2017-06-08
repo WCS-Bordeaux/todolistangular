2c00d9c613de956a74bb2848939cd3a56f0ab046
ignis33 <julien.leotier@gmail.com>
2017-06-08 09:27:17 +0200

    mise en place de mongoose

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -5,13 +5,37 @@ angular.module("todoList")
         templateUrl: "components/home/home.html",
         controller: Home
     })
-function Home($scope) {
+function Home($scope, $http, $resource) {
     $scope.title = "coucou"
     $scope.todoList = [];
-    $scope.addTodo = function (){
-        $scope.todoList.push($scope.todo)
+    $resource("/todo").get().$promise.then(function (R) {
+        for (var i = 0; i < R.todo.length; i++) {
+            $scope.todoList.push(R.todo[i]);
+            console.log("hello")
+        }
+    })
+    $scope.addTodo = function () {
+        if ($scope.todo == "") {
+            return false;
+        }
+        else {
+            let data = {
+                title: $scope.todo
+            }
+            $http.post("/todo", data)
+            $scope.todoList = [];
+            $resource("/todo").get().$promise.then(function (R) {
+                for (var i = 0; i < R.todo.length; i++) {
+                    $scope.todoList.push(R.todo[i]);
+
+                }
+            })
+        }
+        $scope.todo = "";
     }
-    $scope.delete = function(index){
+    $scope.delete = function (index, id) {
+        console.log(id)
+        $http.delete("/todo/" + id)
         $scope.todoList.splice(index, 1);
     }
 }
---
+++ server.js
@@ -0,0 +1,71 @@
+"use strict";
+//Requiere
+const
+    express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    port = process.env.PORT || 3000;
+
+//Utilisation de app, c'est a dire d'express
+app
+    .all('*', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    })
+    .use(express.static('public'))
+    .use(bodyParser.urlencoded({ extended: true }))
+    .use(bodyParser.json());
+
+//On se connect à mongodb
+mongoose.connect('mongodb://localhost/todo');
+//Nouveau schema
+const
+    TodoSchema = mongoose.Schema({
+        title: String
+    }),
+    Todo = mongoose.model('Todo', TodoSchema),
+    routed = express.Router();
+
+
+//API routed
+routed
+    .route('/')
+    .get(function (req, res) {
+        Todo.find(function (err, todo) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({todo});
+        });
+    })
+    .post(function (req, res) {
+        var todo = new Todo();
+        todo.title = req.body.title
+
+        todo.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.send({ message: 'todo created' });
+        })
+    });
+
+routed
+    .route('/:todo_id')
+    .delete(function (req, res) {
+        Todo.remove({ _id: req.params.todo_id }, function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.send({ message: 'todo deleted' });
+        });
+    });
+app.use("/Todo", routed);
+
+app.listen(port, function () {
+    console.log("Adresse du serveur : http://localhost:" + port);
+});
+
+
